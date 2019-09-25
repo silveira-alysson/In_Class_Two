ea5b8b398c6a6cf983319f3876471a81aaf03fdd
Alysson Silveira <alysson.osilveira@gmail.com>
2019-09-24 21:48:13 -0400

    Add project files.

---
+++ In_Class_Two/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace In_Class_Two
+{
+
+        class Person
+        {
+            public static string salutationMr = "Mr";
+            public static string salutationMs = "Ms";
+            public static string defaultsalutationkid = "";
+
+            public string firstName;
+            public string lastName;
+            public string salutation;
+            public Person()
+            {
+                firstName = "Joss";
+                lastName = "Doe";
+                salutation = salutationMs;
+            }
+
+            static void Main(string[] args)
+            {
+                Person pr = new Person();
+
+            }
+        }
+    }
+
