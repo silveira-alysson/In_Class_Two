474d7226445d179760c26828d378ead2074c3efa
Alysson Silveira <alysson.osilveira@gmail.com>
2019-09-24 21:56:42 -0400

    _

---
+++ In_Class_Two/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace In_Class_Two
+{
+    class Physician : Person
+    {
+        public static string salutationDr = "Dr";
+        public static string specialty = "Surgeon";
+
+        public Physician(string salutationMs, string salutationDr, string specialty, string fName, string lName)
+        {
+            Console.Write(salutationMs + salutationDr + specialty + fName + lName);
+
+        }
+
+
+    }
+}
